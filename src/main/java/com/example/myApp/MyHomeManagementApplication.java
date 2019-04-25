8906036c93885d56cf5fbcdbb0657cd64f761e1d
shuchijain123 <sharma.shuchi1@gmail.com>
2019-04-25 14:07:21 -0400

    committing initial shell for app

---
+++ src/main/java/com/example/myApp/MyHomeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.myApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHomeManagementApplication.class, args);
+    }
+
+}
